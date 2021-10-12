95597c2863dbf8e1eb48b71b2c1c08800e241700
Rene Holt <rholtperez@outlook.com>
2021-10-12 23:47:15 +0200

    UPDATE: Preparing to merge with main branch

--- src/main/java/io/inforet/microblog/App.java
+++ src/main/java/io/inforet/microblog/App.java
@@ -5,9 +5,13 @@ import io.inforet.microblog.entities.Query;
 import io.inforet.microblog.tokenization.MicroblogTokenizer;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,6 +20,8 @@ import java.util.function.Function;
 
 public class App {
 
+    public static final String OUTPUT_FILE_ARG = "resultsDir";
+    public static final String resultsFileName = "Results.txt";
     public static final String TREC_DATASET = "trec-dataset.txt";
     public static final String TREC_QUERIES = "trec-queries.xml";
     public static final String STOP_WORDS = "stop-words.txt";
@@ -38,38 +44,64 @@ public class App {
 
 
     public static void main(String[] args) {
+        // (1) Parse the directory argument where the TREC results file will reside
+        String outputPath = System.getProperty(OUTPUT_FILE_ARG);
+        if (outputPath == null || outputPath.isEmpty()) {
+            throw new IllegalArgumentException(String.format("Mandatory argument: '-D%s' not provided...", OUTPUT_FILE_ARG));
+        }
+        try {
+            Path resultsDir = Paths.get(outputPath);
+            if (!Files.isDirectory(resultsDir) || !Files.exists(resultsDir)) {
+                throw new IllegalArgumentException(String.format("The following argument: '-D%s' must be a directory that exists. Provided: '%s'", OUTPUT_FILE_ARG, outputPath));
+            }
+        } catch (InvalidPathException ex) {
+            throw new IllegalArgumentException(String.format("The following argument: '-D%s' must be a well-formed directory that exists. Malformed input provided: '%s'", OUTPUT_FILE_ARG, outputPath));
+        }
+
+        // (2) Parse internal test collection, topics & stop words documents...
         List<InfoDocument> parsedDocuments = (List<InfoDocument>) loadFileEntries(TREC_DATASET, TRECTools::parseCollection);
         List<Query> parsedQueries = (List<Query>) loadFileEntries(TREC_QUERIES, TRECTools::parseQueries);
         Set<String> stopWords = (Set<String>) loadFileEntries(STOP_WORDS, TRECTools::parseStopWords);
         MicroblogTokenizer tokenizer = new MicroblogTokenizer();
-        int totalNumberOfDocuments = parsedDocuments.size();
-        InvertedIndex index = new InvertedIndex(totalNumberOfDocuments);
+        InvertedIndex index = new InvertedIndex(parsedDocuments.size());
 
-        for (InfoDocument document: parsedDocuments) {
+        /// (3) Build Inverted Index
+        for (InfoDocument document : parsedDocuments) {
             String[] tokens = tokenizer.tokenizeDocument(document.getDocument());
-            for(String word: tokens){
+            for (String word : tokens) {
                 //Do not add stopwords to the inverted index
-                if(!stopWords.contains(word)){
-                    index.addToken(word,document.getID());
-                }
-            }           
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Results.txt"))) {
-            for (Query query : parsedQueries) {
-                String[] tokens = tokenizer.tokenizeDocument(query.getQuery());
-                List<Pair<String, Double>> cosineScores = Scoring.cosineScore(index, tokens);
-
-                for (int i = 0; i < cosineScores.size(); i++) {
-                    String docID = cosineScores.get(i).getLeft();
-                    double cosineScore = cosineScores.get(i).getRight();
-
-                    System.out.printf("%s Q0 %s %d %.6f myRun%n", query.getID(), docID, i + 1, cosineScore);
-                    writer.write(String.format("%s Q0 %s %d %.6f myRun%n", query.getID(), docID, i + 1, cosineScore));
+                if (!stopWords.contains(word)) {
+                    index.addToken(word, document.getID());
                 }
             }
         }
-        catch (Exception ex) {System.out.println(ex.getMessage());}
+
+        // (4)  Generate a TREC results file derived from a list of documents scored against a set of executed queries
+        try {
+            String decodedDirPath = URLDecoder.decode(outputPath, "UTF-8");
+            File directoryObj = new File(outputPath);
+            if (!directoryObj.exists() || !directoryObj.isDirectory()) {
+                throw new IllegalArgumentException(String.format("The following file cannot be located or isn't a directory: '%s'", decodedDirPath));
+            }
+            String resultsFilePath = String.format("%s\\%s", decodedDirPath, resultsFileName);
+
+            try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(resultsFilePath))) {
+                for (Query query : parsedQueries) {
+                    String[] tokens = tokenizer.tokenizeDocument(query.getQuery());
+                    List<Pair<String, Double>> cosineScores = Scoring.cosineScore(index, tokens);
+
+                    for (int i = 0; i < cosineScores.size(); i++) {
+                        String docID = cosineScores.get(i).getLeft();
+                        double cosineScore = cosineScores.get(i).getRight();
+
+                        fileWriter.write(String.format("%s Q0 %s %d %,.6f myRun%n", query.getID(), docID, i + 1, cosineScore));
+                    }
+                }
+            } catch (IOException ex) {
+                throw new IllegalArgumentException(String.format("Failed to write to the following results file path: '%s'. Cause: '%s'", resultsFilePath, ex.getCause().getMessage()));
+            }
+        } catch (UnsupportedEncodingException ex) {
+            throw new IllegalArgumentException(String.format("Failed to decode the following file path: '%s'. Cause: '%s'", outputPath, ex.getCause().getMessage()));
+        }
     }
-    
 }
